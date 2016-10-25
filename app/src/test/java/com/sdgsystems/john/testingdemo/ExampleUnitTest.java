eb437eff0f3da459debeef1d96b1ff6c086a109b
jbrimlow <jbrimlow@sdgsystems.com>
2016-10-25 15:48:35 -0400

    first commit

---
+++ app/src/test/java/com/sdgsystems/john/testingdemo/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.sdgsystems.john.testingdemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isNotCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
---
+++ app/src/test/java/com/sdgsystems/john/testingdemo/MainActivityTestSimple2.java
@@ -0,0 +1,15 @@
+package com.sdgsystems.john.testingdemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by john on 10/21/16.
+ */
+public class MainActivityTestSimple2 {
+    @Test
+    public void addition_isNotCorrect() throws Exception {
+        assertEquals(5, 2 + 2);
+    }
+}
